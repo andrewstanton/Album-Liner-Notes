879a1aac0c65ef8e37bdbbd2fae4e5f0b917ed44
webpandadesigns <andrew@toddprod.com>
2022-03-04 13:03:59 -0500

    Storybook configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,25 @@
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import * as NextImage from "next/image";
+
+import GlobalStyle from "../config/GlobalStyle";
+import theme from "../config/theme";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+const withThemeProvider = (Story, context) => (
+  <StyledThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Story {...context} />
+  </StyledThemeProvider>
+);
+
+export const decorators = [withThemeProvider];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
